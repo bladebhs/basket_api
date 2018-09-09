b0d6ee9ef43547986eb65ca14318719fc2192f46
Vladislav Isakov <bladebhs@gmail.com>
2018-09-09 18:42:17 +0500

    Add Product

---
+++ db/migrate/20180909095721_create_products.rb
@@ -0,0 +1,9 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :price, default: 0, null: false
+    end
+  end
+end
