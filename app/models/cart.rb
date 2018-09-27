c0d1b9a5ea36ad41415414f6942e4970720e3980
Vladislav Isakov <bladebhs@gmail.com>
2018-09-27 19:55:04 +0500

    Add `add-product`, `remove-product`, `as_json` and `size` to Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,9 +6,37 @@ class Cart
     @cart_items = []
   end
 
-  # TODO
-  def add_product(cart_item) end
+  def add_product(cart_item)
+    if existing_item = cart_items.detect { |item| item.product_id == cart_item.product_id }
+      existing_item.quantity += cart_item.quantity
+      existing_item.calculate_sum
+    else
+      cart_items << cart_item
+    end
+  end
 
-  # TODO
-  def remove_product(product_id) end
+  def remove_product(product_id)
+    found_item = cart_items.detect { |item| item.product_id == product_id }
+
+    if found_item.quantity > 1
+      found_item.quantity -= 1
+      found_item.calculate_sum
+    else
+      cart_items.delete_if { |item| item.product_id == product_id }
+    end
+  end
+
+  def size
+    cart_items.size
+  end
+
+  def as_json(options = nil)
+    {
+      data: {
+        total_sum: cart_items.map { |p| p.sum }.reduce(0, :+),
+        products_count: size,
+        products: cart_items
+      }
+    }
+  end
 end
