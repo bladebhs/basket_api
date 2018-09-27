b12e547e3c9653fd777c0df1b4cd18f2f381ce41
Vladislav Isakov <bladebhs@gmail.com>
2018-09-27 21:18:08 +0500

    Refactor `Cart API` spec

--- spec/requests/cart_spec.rb
+++ spec/requests/cart_spec.rb
@@ -20,35 +20,15 @@ end
 
 describe 'Cart API', type: :request do
   let(:cart) { Cart.instance }
-  let!(:products) { create_list(:product, 2) }
+  #let!(:products) { create_list(:product, 2) }
+  #let(:valid_item) { { product_id: products.first.id, quantity: 2 } }
+  #let(:invalid_item) { { product_id: products.first.id, quantity: 20 } }
+  let(:valid_item) { attributes_for(:cart_item) }
+  let(:invalid_item) { attributes_for(:cart_item, quantity: 20) }
 
   describe 'POST /api/cart' do
-    let(:valid_item) { { product_id: products.first.id, quantity: 2 } }
-    let(:other_valid_item) { { product_id: products.second.id, quantity: 5 } }
-    let(:invalid_item) { { product_id: products.first.id, quantity: 20 } }
-
-    before { cart.add_product valid_item }
-
     context 'with valid parameters' do
-      context 'when the product is new' do
-        before { post '/api/cart', params: other_valid_item }
-
-        it 'adds it to the cart' do
-          expect(cart.cart_items.size).to eq 2
-        end
-      end
-
-      context 'when the product is already exist' do
-        before { post '/api/cart', params: valid_item }
-
-        it 'increases its quantity' do
-          expect(cart.cart_items.first[:quantity]).to eq(valid_item[:quantity] * 2)
-        end
-
-        it 'recalculates the sum' do
-          expect(cart.cart_items.first[:sum]).to eq(cart.cart_items.first[:sum] * 2)
-        end
-      end
+      before { post '/api/cart', params: valid_item }
 
       it_behaves_like 'a valid request'
     end
@@ -72,5 +52,11 @@ describe 'Cart API', type: :request do
 
       it_behaves_like 'an invalid request'
     end
+  end
+
+  describe 'GET /api/cart' do
+    before { get '/api/cart' }
+
+    it_behaves_like 'a valid request'
   end
 end
